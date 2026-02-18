e7801341b3c6f92fae69fdf5f77fb3ba94b8e9af
dmycse <dmycse@gmail.com>
2026-02-18 12:12:27 +0200

    feat(components/header): create NavLinks

--- src/components/header/NavDesktop.tsx
+++ src/components/header/NavDesktop.tsx
@@ -1,4 +1,4 @@
-import { useActiveLink } from "@/hooks/useActiveLink";
+import NavLinks from "@/components/header/NavLinks";
 
 type NavDesktopProps = {
   links: {
@@ -11,28 +11,14 @@ type NavDesktopProps = {
 };
 
 export default function NavDesktop({ links, className, children }: NavDesktopProps) {
-  const activeHash = useActiveLink();
-
+  
   return (
     <nav class={`${className} flex items-center gap-4 md:gap-8`}>
-      <ul class="flex gap-0">
-        {links.map(link => link.visible && (
-          <li class="after:content-['/'] after:mx-4 last:after:content-none">
-            <a 
-              href={link.name === "Home" ? "/" : `#${link.path}`}
-              class={`text-base uppercase tracking-wide 
-                      ${activeHash === `#${link.path}` 
-                        ? "text-amber-300" 
-                        : activeHash === "" && link.name === "Home" 
-                          ? "text-amber-300"
-                          : "text-white"}
-                    `}
-            >
-              {link.name}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <NavLinks 
+        links={links} 
+        ulStyle="flex gap-0"
+        liStyle="after:content-['/'] after:mx-2 last:after:content-none"
+      />
       {children}
     </nav>
   )
--- src/components/header/NavMobile.tsx
+++ src/components/header/NavMobile.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "preact/hooks";
-import { useActiveLink } from "@/hooks/useActiveLink";
 import NavMobileBtn from "@/components/header/NavMobileBtn";
+import NavLinks from "@/components/header/NavLinks";
 
 type NavMobileProps = {
   links: {
@@ -17,9 +17,8 @@ type NavMobileProps = {
 
 export default function NavMobile({ links, open, close, className, children }: NavMobileProps) {
   const [isOpen, setIsOpen] = useState(false);
-
-  const activeHash = useActiveLink(() => setIsOpen(false));
   
+  // disable scroll on body while menu is open
   useEffect(() => {
     document.body.style.overflow = isOpen ? "hidden" : "";
     
@@ -29,7 +28,7 @@ export default function NavMobile({ links, open, close, className, children }: N
   }, [isOpen]);
 
   return (
-    <nav class={className}>
+    <div class={className}>
       <NavMobileBtn 
         isOpen={isOpen} 
         setIsOpen={setIsOpen} 
@@ -37,39 +36,26 @@ export default function NavMobile({ links, open, close, className, children }: N
         close={close} 
       />
       <div 
-        class={`fixed inset-0 z-10 bg-black/40 transition-opacity duration-600
+        className={`fixed inset-0 z-10 bg-black/40 transition-opacity duration-600
                 ${isOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"}
               `}
         onClick={() => setIsOpen(false)}
       />
-      <aside
+      <nav
         id="mobile-menu"
-        class={`w-full h-dvh md:h-[50svh] landscape:h-svh absolute inset-x-0 top-full z-40
+        className={`w-full h-dvh md:h-[50svh] landscape:h-svh absolute inset-x-0 top-full z-40
                 flex flex-col items-center bg-primary border-t border-tertiary
                 transition-transform duration-800 ease-out 
                 ${isOpen ? "translate-y-0 pointer-events-auto" : "-translate-y-[200%] pointer-events-none"}
         `}
       >
-        <ul class="p-12 flex items-center flex-col landscape:flex-row gap-y-12 sm:gap-x-10 text-center">
-          {links.map(link => link.visible && (
-            <li key={link.name}>
-              <a 
-                href={link.name === "Home" ? "/" : `#${link.path}`}
-                className={`text-xl uppercase tracking-wider
-                            ${activeHash === `#${link.path}` 
-                              ? "text-amber-300 font-semibold" 
-                              : activeHash === "" && link.name === "Home" 
-                                ? "text-amber-300 font-semibold"
-                                : "text-white font-normal"}
-                          `}
-              >
-                {link.name}
-              </a>
-            </li>
-          ))}
-        </ul>
+        <NavLinks 
+          links={links} 
+          onHashChange={() => setIsOpen(false)}
+          ulStyle="p-12 flex items-center flex-col landscape:flex-row gap-y-12 sm:gap-x-10 text-center"
+        />
         {children}
-      </aside>
-  </nav>
+      </nav>
+  </div>
   )
 }
