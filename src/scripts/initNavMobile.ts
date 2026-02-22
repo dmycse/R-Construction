f5238ea5833632589e34ef8cfa6929fb2bfb4917
dmycse <dmycse@gmail.com>
2026-02-22 17:48:10 +0200

    refactor(scripts): refactor initNavMobile completly

--- src/scripts/initNavMobile.ts
+++ src/scripts/initNavMobile.ts
@@ -1,71 +1,32 @@
-export function initNavMobile(btnSelector: string, menuSelector: string, overlaySelector: string) {
+export function initNavMobile(menuContainer: HTMLElement) {
 
-  const btn = document.querySelector(btnSelector);
-  const menu = document.querySelector(menuSelector);
-  const overlay = document.querySelector(overlaySelector);
+  const btn = menuContainer.querySelector('button');
+  const overlay = menuContainer.querySelector('[data-menu-overlay]');
+  if (!btn || !overlay) return;
 
-  if (!btn || !menu || !overlay) return;
-
-  let isOpen = false;
-
-  const openMenu = () => {
-    isOpen = true;
-    menu.classList.remove('-translate-y-[200%]', 'pointer-events-none');
-    menu.classList.add('translate-y-0', 'pointer-events-auto');
-
-    overlay.classList.remove('opacity-0', 'pointer-events-none');
-    overlay.classList.add('opacity-100', 'pointer-events-auto');
-
-    document.body.style.overflow = 'hidden';
-    updateBtnState();
+  const toggleMenu = (forceState?: boolean) => {
+    const menuOpen = forceState ?? menuContainer.dataset.menuOpen !== 'true';
+    
+    menuContainer.setAttribute('data-menu-open', menuOpen ? 'true' : 'false');
+    btn.setAttribute('aria-expanded', menuOpen ? 'true' : 'false');
+    document.body.style.overflow = menuOpen ? 'hidden' : '';
   };
 
-  const closeMenu = () => {
-    isOpen = false;
-    menu.classList.remove('translate-y-0', 'pointer-events-auto');
-    menu.classList.add('-translate-y-[200%]', 'pointer-events-none');
+  btn.addEventListener('click', () => toggleMenu());
+  overlay.addEventListener('click', () => toggleMenu(false));
 
-    overlay.classList.remove('opacity-100', 'pointer-events-auto');
-    overlay.classList.add('opacity-0', 'pointer-events-none');
-
-    document.body.style.overflow = '';
-    updateBtnState();
-  };
-
-  const updateBtnState = () => {
-    const openIcon = btn.querySelector('[data-icon-open]') as HTMLElement;
-    const closeIcon = btn.querySelector('[data-icon-close]') as HTMLElement;
-
-    if (isOpen) {
-      openIcon.classList.add('opacity-0', 'rotate-90', 'scale-50');
-      openIcon.classList.remove('opacity-100', 'rotate-0', 'scale-100');
-
-      closeIcon.classList.add('opacity-100', 'rotate-0', 'scale-100');
-      closeIcon.classList.remove('opacity-0', '-rotate-90', 'scale-50');
-
-    } else {
-      openIcon.classList.add('opacity-100', 'rotate-0', 'scale-100');
-      openIcon.classList.remove('opacity-0', 'rotate-90', 'scale-50');
-
-      closeIcon.classList.add('opacity-0', '-rotate-90', 'scale-50');
-      closeIcon.classList.remove('opacity-100', 'rotate-0', 'scale-100');
-    }
-  };
-
-  // handlers click on btn and overlay
-  btn.addEventListener('click', () => (isOpen ? closeMenu() : openMenu()));
-  overlay.addEventListener('click', closeMenu);
-
-  // delegation clicks on a tag with data-close-menu attribute
-  document.addEventListener('click', (e) => {
-    if (!(e.target instanceof HTMLElement)) return;
-
-    const link = e.target.closest('[data-close-menu]');
-    if (link && isOpen) {
-      closeMenu();
+  // close menu by click on links
+  menuContainer.addEventListener('click', (e) => {
+    const target = e.target as HTMLElement;
+    if (target.closest('[data-menu-close]')) {
+      toggleMenu(false);
     }
   });
 
-  // init at page load
-  updateBtnState();
+  // close menu on resize
+  window.addEventListener('resize', () => {
+    if (window.innerWidth >= 1024 && menuContainer.dataset.menuOpen === 'true') {
+      toggleMenu(false);
+    }
+  });
 }
