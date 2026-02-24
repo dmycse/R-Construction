107aa3161394dfb8c58124833fb3b887a38cc7db
dmycse <dmycse@gmail.com>
2026-02-24 09:59:26 +0200

    feat(data): create data for projects section

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 import { defineCollection, z } from "astro:content";
-import type { ImageFunction, InferEntrySchema } from 'astro:content';
+import type { InferEntrySchema } from 'astro:content';
 
 export const topbarData = defineCollection({
   type: 'data',
@@ -112,6 +112,33 @@ export const servicesData = defineCollection({
   })
 });
 
+export const projectsData = defineCollection({
+  type: 'data',
+  schema: ({ image })  => z.object({
+    title: z.string(),
+    sectionId: z.string(),
+    sectionTitle: z.string(),
+    sectionSubtitle: z.string(),
+    sectionText: z.array(z.string()),
+    ctaText: z.string(),
+    ctaBtn: z.object({
+      visible: z.boolean(),
+      label: z.string(),
+      path: z.string(),
+      icon: z.string().optional()
+    }),
+    projectsData: z.array(z.object({
+      id: z.string(),
+      name: z.string(),
+      title: z.string(),
+      icon: z.string(),
+      image: image(),
+      description: z.string(),
+      worksList: z.array(z.string())
+    }))
+  })
+});
+
 export const collections = {
   'topbar': topbarData,
   'header': headerData,
@@ -119,10 +146,11 @@ export const collections = {
   'about': sectionData,
   'stats': statsData,
   'services': servicesData,
-  'projects': sectionData
+  'projects': projectsData
 };
 
 type TobBarSchema = InferEntrySchema<'topbar'>;
 export type ContactsType = TobBarSchema['contacts'];
 export type StatsDataSchema = InferEntrySchema<'stats'>;
 export type ServicesDataSchema = InferEntrySchema<'services'>;
+export type ProjectsDataSchema = InferEntrySchema<'projects'>;
