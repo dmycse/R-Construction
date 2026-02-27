11605be81e247dcdb9b55a1997eebfeff92212b7
dmycse <dmycse@gmail.com>
2026-02-27 11:10:29 +0200

    refactor(scripts): refactor animateElements

--- src/scripts/animateElements.ts
+++ src/scripts/animateElements.ts
@@ -1,28 +1,20 @@
-function animateElements() {
-  const elements = document.querySelectorAll(".fade-in-y, .fade-in-left, .fade-in-right");
+export function animateElements() {
+  const elements = document.querySelectorAll<HTMLElement>(".fade-in-y, .fade-in-left, .fade-in-right");
 
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       const el = entry.target as HTMLElement;
 
-      const animationClass = [...el.classList].find(className => className.startsWith("fade-"));
-      if (!animationClass) return;
-
-      const activeClass = animationClass.replace("fade-in", "fade-up");
-
       if (entry.isIntersecting) {
-        el.classList.add(activeClass);
+        el.dataset.inview= "true";
+        // cansel animation once the element has been in view
+        observer.unobserve(el);
+      } else {
+        el.dataset.inview= "false";
       }
-      else {
-        el.classList.remove(activeClass);
-      }
-      
-      // observer.unobserve(entry.target);
 
     });
   }, { threshold: 0.20 });
 
   elements.forEach(element => observer.observe(element));
 }
-
-animateElements();
